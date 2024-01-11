1cdaf71b251ec4c8bf286bd2f5f89d088dc9ba58
Amberjack10 <151727593+Amberjack10@users.noreply.github.com>
2024-01-11 09:48:19 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        if((num1 <= 0 || num1 > 100) || (num2 <= 0 || num2 > 100)) throw new Exception("invaild param");
+        int answer = num1 % num2;
+        return answer;
+    }
+}
