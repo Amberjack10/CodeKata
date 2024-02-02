5fa1aa0ebe032dd166dd30cfbda05979fc34c360
Amberjack10 <151727593+Amberjack10@users.noreply.github.com>
2024-02-02 09:42:58 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.29 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] solution(long n) {
+        int[] temp = new int[11] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+        int i = 0;
+        
+        for(i = 0; n > 0; i++, n /= 10){
+            temp[i] = (int) (n % 10);
+        }
+        
+        int[] answer = new int[i];
+        
+        for(int j = 0; j < i; j++){
+            answer[j] = temp[j];
+        }
+        
+        return answer;
+    }
+}
