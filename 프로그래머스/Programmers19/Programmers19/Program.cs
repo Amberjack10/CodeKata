7430e021ecba3f468c94bb0f2299e002ffdd74bf
Amberjack10 <151727593+Amberjack10@users.noreply.github.com>
2024-02-06 10:22:44 +0900

    Programmers 19. 정수 제곱근 판별

---
+++ 프로그래머스/Programmers19/Programmers19/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Programmers19
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public long solution(long n)
+            {
+                long answer = 0;
+
+                answer = (long)Math.Sqrt(n);
+
+                if(answer * answer == n)
+                {
+                    return answer;
+                }
+
+                return -1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine($"{solution.solution(49000000000000)}");
+        }
+    }
+}
