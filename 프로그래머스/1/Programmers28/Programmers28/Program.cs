5547de4fb740f5cfe718045c3a802d274ff13141
Amberjack10 <151727593+Amberjack10@users.noreply.github.com>
2024-02-21 09:11:39 +0900

    Programmgers 28

---
+++ 프로그래머스/1/Programmers28/Programmers28/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Programmers28
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int[] numbers)
+            {
+                int answer = 45;
+
+                foreach (int i in numbers)
+                {
+                    answer -= i;
+                }
+
+                return answer;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.solution([1,2,3,4,6,7,8,0]));
+        }
+    }
+}
