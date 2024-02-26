15915020275b84825c2400851b944132f1dcd56d
Amberjack10 <151727593+Amberjack10@users.noreply.github.com>
2024-02-26 20:21:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.26 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -4,7 +4,7 @@ public class Solution
 {
     public string solution(int n)
     {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(10001);
         bool flag = true;
 
         for (int i = 0; i < n; i++)
