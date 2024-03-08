e041a4fdadaef56217f44292c28ec91e71606028
Amberjack10 <151727593+Amberjack10@users.noreply.github.com>
2024-03-08 09:17:53 +0900

    [level 1] Title: 3진법 뒤집기, Time: 1.04 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        List<int> list = new List<int>();
+
+        while (n != 0)
+        {
+            list.Add(n % 3);
+            n /= 3;
+        }
+
+        list.Reverse();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            answer += (int)(list[i] * Math.Pow(3, i));
+        }
+        return answer;
+    }
+}
